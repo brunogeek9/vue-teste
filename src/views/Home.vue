fcf6eba5277ac63f5b633dbcc17a0d6fcb58f031
brunogeek9 <brunogeek9@gmail.com>
2020-05-13 12:35:10 -0300

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <div id="box2" class="container">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque sequi repellat fugiat facere reiciendis alias aliquid odit voluptates quae nulla minima, officiis cumque esse nihil laborum reprehenderit porro consectetur.</p>
+    </div>
+    <div id="box2" class="container">
+      <p>
+        <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ea perferendis, sapiente blanditiis eveniet est, officia quam maiores, fugit ratione placeat consequatur alias amet. Aspernatur fugiat inventore impedit corrupti architecto.</strong>
+      </p>
+    </div>
+    <div id="box2" class="container">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque sequi repellat fugiat facere reiciendis alias aliquid odit voluptates quae nulla minima, officiis cumque esse nihil laborum reprehenderit porro consectetur.</p>
+    </div>
+    <div id="box2" class="container">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque sequi repellat fugiat facere reiciendis alias aliquid odit voluptates quae nulla minima, officiis cumque esse nihil laborum reprehenderit porro consectetur.</p>
+    </div>
+    <div id="box2" class="container">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque sequi repellat fugiat facere reiciendis alias aliquid odit voluptates quae nulla minima, officiis cumque esse nihil laborum reprehenderit porro consectetur.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  components: {
+  }
+};
+</script>
+<style scoped>
+.container {
+  
+  display: flex;
+  flex-direction: row-reverse ;
+}
+
+</style>
